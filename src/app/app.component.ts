8e1e9b31b6564ec095f7ce908eb006e03d49fa36
matteson <andrew.matteson@appliedbiomath.com>
2018-05-09 15:24:18 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  dataEndpoint: any;
+  layoutEndpoint: any;
+
+  dataTraces: any;
+  layoutTraces: any;
+
+  visibleTraces: any;
+
+  ngOnInit(): void {
+    let endpointTrace = {
+      x: [1, 2, 3, 4, 5, 6],
+      y: [100, 90, 70, 40, 20, 10],
+      type: 'scatter',
+      visible: true,
+    };
+
+    this.dataEndpoint = [endpointTrace];
+    this.layoutEndpoint = {
+      'title': 'Endpoints',
+      'showlegend': true
+    };
+
+    let dataTrace1 = {
+      x: [ 1, 2, 3, 4],
+      y: [100, 100, 100, 100],
+      type: 'scatter',
+      visible: true
+    };
+    let dataTrace2 = {
+      x: [ 1, 2, 3, 4],
+      y: [100, 100, 95, 90],
+      type: 'scatter',
+      visible: false
+    };
+    let dataTrace3 = {
+      x: [ 1, 2, 3, 4],
+      y: [100, 95, 90, 70],
+      type: 'scatter',
+      visible: false
+    };
+    let dataTrace4 = {
+      x: [ 1, 2, 3, 4],
+      y: [95, 90, 70, 40],
+      type: 'scatter',
+      visible: false
+    };
+    let dataTrace5 = {
+      x: [ 1, 2, 3, 4],
+      y: [80, 65, 40, 20],
+      type: 'scatter',
+      visible: false
+    };
+    let dataTrace6 = {
+      x: [ 1, 2, 3, 4],
+      y: [60, 35, 20, 10],
+      type: 'scatter',
+      visible: true
+    };
+
+    this.dataTraces = [dataTrace1, dataTrace2, dataTrace3, dataTrace4, dataTrace5, dataTrace6];
+    this.layoutTraces = {
+      'title': 'Traces',
+      'showlegend': true
+    };
+  }
+
+  hover(event: any): void {
+    console.log(event);
+    const ind = event['points'][0]['pointIndex'];
+    this.dataTraces[ind].visible = true;
+  }
+
+  unhover(event: any): void {
+    this.dataTraces[0].visible = true;
+    this.dataTraces[1].visible = false;
+    this.dataTraces[2].visible = false;
+    this.dataTraces[3].visible = false;
+    this.dataTraces[4].visible = false;
+    this.dataTraces[5].visible = true;
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { PlotlyModule } from 'angular-plotly.js';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    PlotlyModule
   ],
   providers: [],
   bootstrap: [AppComponent]
